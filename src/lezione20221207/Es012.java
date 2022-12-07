562da20323ebbcf7aacf5a5c9922193538e270c1
Luca Pennini <luca198871@gmail.com>
2022-12-07 15:51:28 +0100

    aggiornamento con esercizio 12 sulla somma dei primi x numeri e 13 per ritrovare il numero assoluto

---
+++ src/lezione20221207/Es012.java
@@ -0,0 +1,25 @@
+package lezione20221207;
+
+import java.util.Scanner;
+
+public class Es012 {
+    /**
+     * Somma dei primi x numeri
+     * @param n primi numeri da sommare
+     * @return somma dei primi x numeri
+     */
+    public static int sum(int n){
+        int result = 0;
+        for (int i = 0; i <= n; i++) {
+            result = result + i;
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("inserisci il valore dell'ultimo valore da sommare");
+        int numero1 = scanner.nextInt();
+        System.out.println(sum(numero1));
+
+    }
+}
---
+++ src/lezione20221207/Es013.java
@@ -0,0 +1,23 @@
+package lezione20221207;
+
+import java.util.Scanner;
+
+public class Es013 {
+    /**
+     * Program to return the absolute of a number
+     * @param n number to be converted
+     * @return the abs of the number in the input
+     */
+    public static double abs(double n){
+        if (n<=0){
+            n = n*-1;
+        }
+        return n;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("inserisci il valore da rendere assoluto");
+        double numero1 = scanner.nextDouble();
+        System.out.println(abs(numero1));
+    }
+}
