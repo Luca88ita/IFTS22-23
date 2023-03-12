4e8075853621bfea42195868ec95cba975b34353
Luca88ita <luca198871@gmail.com>
2023-03-12 13:53:23 +0100

    up

--- src/home/mazziotti - website/scripts/index.js
+++ src/home/mazziotti - website/scripts/index.js
@@ -1,5 +1,7 @@
+// in questo script vado ad importare la classe Header per poterla utilizzare
 import Header from "./modules/header.js";
 
+// qui la vado a richiamare dandole tutti gli attributi di cui necessita per poter funzionare bene (vedi come è fatta la classe Header che ho creato e di che parametri ha bisogno)
 Header(
   "../mazziotti - website/assets/banner.jpeg",
   "../mazziotti - website/assets/logo.jpg",
