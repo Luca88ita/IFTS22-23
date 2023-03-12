11b9a50144bbdb9e9ce78ec3c7470fa88c180b10
Luca88ita <luca198871@gmail.com>
2023-03-12 13:49:32 +0100

    uploaded example of creation of DOM via JS

---
+++ src/home/mazziotti - website/scripts/index.js
@@ -0,0 +1,7 @@
+import Header from "./modules/header.js";
+
+Header(
+  "../mazziotti - website/assets/banner.jpeg",
+  "../mazziotti - website/assets/logo.jpg",
+  "Associazione ci piacciono i gatti"
+);
---
+++ src/home/mazziotti - website/scripts/modules/header.js
@@ -0,0 +1,41 @@
+// vado a creare una funzione che ho deciso di chiamare Header (perché la utilizzerò per creare l'header e gli elementi al suo interno)
+// Per poter riutilizzare questa funzione, vado a dargli degli attributi, che sono:
+// bannerUrl che è l'indirizzo dell'immagine del banner
+// logoUrl che è l'indirizzo dell'immagine del logo
+// title che è il titolo che voglio dare al mio header
+//
+// quando si andrà a richiamare questa funzione, per farla lavorare correttamente, bisognerà fornirle tutti e 3 questi parametri
+
+const Header = (bannerUrl, logoUrl, title) => {
+  // vado a richiamare l'elemento root presente nella pagina html
+  let root = document.getElementById("root");
+  // vado a creare l'elemento header...
+  let header = document.createElement("header");
+  // ... e qui vado ad "appenderlo" all'elemento root richiamato in precedenza
+  root.appendChild(header);
+
+  // qui vado invece a creare l'immagine del banner, con tutte le sue proprietà
+  let banner = document.createElement("img");
+  banner.className = "banner";
+  banner.src = bannerUrl;
+  banner.alt = "banner";
+  // ed infine lo appendo all'elemento header
+  header.appendChild(banner);
+
+  // qui vado a creare in maniera simile l'immagine del logo, con tutte le sue proprietà
+  let logo = document.createElement("img");
+  logo.className = "logo";
+  logo.src = logoUrl;
+  logo.alt = "logo";
+  // e lo appendo all'elemento header
+  header.appendChild(logo);
+
+  // e come ultima cosa, vado a creare l'elemento h1 dove avrò il titolo
+  let h1 = document.createElement("h1");
+  // gli vado a dare un testo...
+  h1.textContent = title;
+  // e per completare l'opera, lo appendo all'elemento header
+  header.appendChild(h1);
+};
+
+export default Header;
