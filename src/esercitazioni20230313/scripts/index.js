29484c47cfbd85fe22401612514333ec152fb310
Luca88ita <luca198871@gmail.com>
2023-03-14 09:34:15 +0100

    modified button bg

--- src/esercitazioni20230313/scripts/index.js
+++ src/esercitazioni20230313/scripts/index.js
@@ -2,9 +2,9 @@ const root = document.getElementById("root");
 
 const header = document.createElement("header");
 header.style.backgroundImage = "url(./assets/rainbow.jpg)";
-header.style.backgroundSize= "fill";
-header.style.backgroundPosition= "left";
-header.style.backgroundRepeat="no-repeat";
+header.style.backgroundSize = "fill";
+header.style.backgroundPosition = "left";
+header.style.backgroundRepeat = "no-repeat";
 root.appendChild(header);
 
 const title = document.createElement("h1");
@@ -31,26 +31,28 @@ for (let index = 0; index < 4; index++) {
   linkArray[index].appendChild(buttonArray[index]);
 }*/
 
-
 for (let index = 0; index < 4; index++) {
   let aLink = document.createElement("a");
-  aLink.id = "links"+(index);
+  aLink.id = "links" + index;
   aLink.className = "links";
-  aLink.href="https://www.google.com/it"
-  aLink.target="_blank";
+  aLink.href = "https://www.google.com/it";
+  aLink.target = "_blank";
   //link1.target="_self";
   navBar.appendChild(aLink);
 
   let linkedButton = document.createElement("button");
-  linkedButton.textContent = "Link "+(index+1);
-  linkedButton.style.cssText = "transition: 1000ms"
-  linkedButton.addEventListener("mouseover",() =>{
-    linkedButton.style.backgroundColor="yellow";
-  })
-  linkedButton.addEventListener("mouseout",() =>{
-    linkedButton.style.backgroundColor="white";
-  })
+  linkedButton.textContent = "Link " + (index + 1);
+  linkedButton.style.backgroundColor = "white";
+  linkedButton.style.cssText = "transition: 1000ms";
+  linkedButton.addEventListener("mouseover", () => {
+    linkedButton.style.backgroundColor = "yellow";
+    linkedButton.style.backgroundImage = "none";
+  });
+  linkedButton.addEventListener("mouseout", () => {
+    linkedButton.style.backgroundColor = "white";
+    linkedButton.style.backgroundImage = "url(./assets/rainbow.jpg)";
+    linkedButton.style.backgroundSize = "cover";
+    linkedButton.style.backgroundRepeat = "no-repeat";
+  });
   aLink.appendChild(linkedButton);
-
 }
-
